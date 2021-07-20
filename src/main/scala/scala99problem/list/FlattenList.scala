acbf9b700a1ed742849f72dd1d881f3c2136108f
shishir <shishirkumar28@gmail.com>
2021-07-20 10:29:23 +0800

    reverted refactor

--- src/main/scala/scala99problem/list/FlattenList.scala
+++ src/main/scala/scala99problem/list/FlattenList.scala
@@ -5,6 +5,8 @@ class FlattenList {
 
     def flatten_(list: List[Any], acc: List[Any]): List[Any] = list match {
       case Nil => acc
+      case (x:List[Any]) :: Nil   =>  flatten_(x, acc)
+      case (x:List[Any]) :: tail  => flatten_(tail, acc ::: x)
       case x :: tail              => flatten_(tail, acc ::: List(x))
     }
 
